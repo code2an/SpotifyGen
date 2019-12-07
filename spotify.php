d1f630bf97ab26d7c8d5792de70a4b49b8b21151
Iqbal Mandiyansah <56031836+code2an@users.noreply.github.com>
2019-12-07 10:47:37 +0700

    Add files via upload

---
+++ spotify.php
@@ -0,0 +1,55 @@
+<?php
+
+@system("clear");
+$blue="\033[1;34m";
+$cyan="\033[1;36m";
+$okegreen="\033[92m";
+$lightgreen="\033[1;32m";
+$white="\033[1;37m";
+$purple="\033[1;35m";
+$red="\033[1;31m";
+$yellow="\033[1;33m";
+
+################################
+$banner     = "
+......00000000000...... ||
+....000000000000000.... ||
+...000........000000... || Author  : Iqbal Mandiyansah
+..000..000000.....000.. || Version : 1.0
+..00000......00000000.. || Create  : 07 Desember 2019
+..00000000000,...0000.. || Name    : Spotify Toolkit
+..00000.......0000000.. || https://github.com/code2an/SpotifyGen
+...00000000000..0000... || ## Spotify - Music For Everyone
+....000000000000000.... ||
+......00000000000...... ||
+[!] Spotify is a digital music streaming service, I made several tools for you.
+";
+print $banner;
+print "\n";
+print "$yellow ->$white Let's generate some$okegreen Spotify Premium$white Account\n";
+print "\n";
+print "$okegreen ??$white Total : ";
+$jumlah = trim(fgets(STDIN));
+print "\n";
+print "$red //$white Generating $jumlah Account\n";
+print "\n";
+$result = file_get_contents('http://api.kode2an.best/api/spotify/?jumlah='.$jumlah);
+$json = json_decode($result, true);
+foreach ((array) $json as $row){
+    $country = $row['Country'];
+    $type = $row['Account Type'];
+    $email = $row['Email'];
+    $pass = $row['Password'];
+    $exp = $row['Expired'];
+    print "$okegreen **$white Type    : $type\n";
+    print "$okegreen **$white Email   : $email\n";
+    print "$okegreen **$white Pass    : $pass\n";
+    print "$okegreen **$white Country : $country \n";
+    if ($exp == ''){
+        print "\n";
+    }
+    else{
+        print "$okegreen **$white Expired : $exp\n";
+        print "\n";
+    }
+}
